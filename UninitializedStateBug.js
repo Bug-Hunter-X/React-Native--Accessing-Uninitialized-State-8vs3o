15cf9310f8d20b3d49e754c92e1962dee1f00187
BugHunter-X <askzenai@gmail.com>
2025-02-17 15:16:05 +0100

    Add UninitializedStateBug.js file

---
+++ UninitializedStateBug.js
@@ -0,0 +1,1 @@
+This error occurs when you try to access a state variable before it has been initialized. This often happens when you try to access state within a component's render method before the component has mounted and the state has been set.
